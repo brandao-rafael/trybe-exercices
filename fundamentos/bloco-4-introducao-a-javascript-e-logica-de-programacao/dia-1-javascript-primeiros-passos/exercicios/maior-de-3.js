64efdcf47b095d052bdd0f9842cf580f1768ef95
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-12 18:30:11 -0300

    exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/maior-de-3.js
@@ -0,0 +1,11 @@
+const n1 = 2;
+const n2 = 1;
+const n3 = 1;
+
+if (n1 > n2 && n1 > n3){
+    console.log(n1);
+} else if (n2 > n1 && n2 > n3) {
+    console.log(n2);
+} else {
+    console.log(n3)
+}
