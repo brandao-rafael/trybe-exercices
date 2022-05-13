97458e4aee19d289c89a96aa27db37b120d72e4e
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-13 11:14:36 -0300

    Primeiro exercicio do conteúdo

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-dos-conteudos/exercicio-1.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+let indexOfPortfolio = menu.indexOf('Portifólio');
+
+menu.push('Contato')
+
+console.log(menuServices);
+console.log(indexOfPortfolio);
+console.log(menu);
