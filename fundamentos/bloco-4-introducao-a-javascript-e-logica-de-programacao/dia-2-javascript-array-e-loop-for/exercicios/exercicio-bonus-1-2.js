536bdc28c2099c16a89920df415e75634656f502
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-13 18:34:49 -0300

    finalizando exercicios do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/exercicio-bonus-1-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/exercicio-bonus-1-2.js
@@ -1,7 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 1; i < numbers.length; i += 1) {
+  console.log("i");
   for (let j = 0; j < i; j += 1) {
+    console.log('j');
     if (numbers[i] < numbers[j]) {
       let positionChange = numbers[i];
       numbers[i] = numbers[j];
