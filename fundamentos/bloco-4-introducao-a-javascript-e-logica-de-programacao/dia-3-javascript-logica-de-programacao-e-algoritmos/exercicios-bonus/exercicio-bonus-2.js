1ffc95c4f1c46f1696a73789e39a04a2ea07a1f9
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-16 19:17:45 -0300

    exercicio bonus 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-bonus/exercicio-bonus-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-bonus/exercicio-bonus-2.js
@@ -1,10 +1,10 @@
 let number = 5;
-let quadrado = '';
+let triangulo = '';
 for (let i = 1; i <= number; i += 1){ 
     for (let j = 1; j <= number; j += 1) {
         if (j === i) {
-            quadrado += '*';
+            triangulo += '*';
         }
     }
-    console.log(quadrado);
+    console.log(triangulo);
 }
