99918cb5fa2a0486bd003f36642d5c05d38f68fd
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-16 18:23:04 -0300

    exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/maior-menor-palavra-exercicio-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/maior-menor-palavra-exercicio-3.js
@@ -1,14 +1,16 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 let maior = '';
-let menor = '';
+let menor = array[0];
 for (let i = 0; i < array.length; i += 1) {
-    for (let j = 0; j < i; j += 1) {
-        if (array[i].length > array[j].length) {
-            maior = array[i];
-        } else {
-            menor = array[j]
-        }
-        console.log(array[i], array[j])
+    if (array[i].length > maior.length){
+        maior=array[i];
+        console.log('if')
+    }; 
+    if (array[i].length < menor.length){
+        menor=array[i];
+        console.log('else');
     }
+    
 }
-console.log(maior, menor)
+console.log('maior: '+ maior);
+    console.log('menor: ' + menor); 
