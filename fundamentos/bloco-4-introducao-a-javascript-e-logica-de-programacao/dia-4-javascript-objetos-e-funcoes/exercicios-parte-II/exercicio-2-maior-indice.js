9838523b04a809c57b94eba6f30426226c48ee08
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-17 19:00:42 -0300

    exercicio 2 parte 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-II/exercicio-2-maior-indice.js
@@ -0,0 +1,14 @@
+
+function biggestIndex (number){
+    let bigIndex = 0;
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] > number[bigIndex]){
+            bigIndex = i;
+        }
+        
+    }
+    return bigIndex
+}
+let testArray =  [2, 3, 6, 17, 10, 1];
+console.log(biggestIndex(testArray))
+
