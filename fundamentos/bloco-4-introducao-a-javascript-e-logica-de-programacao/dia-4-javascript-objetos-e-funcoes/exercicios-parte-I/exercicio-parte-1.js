8aedfeaf888fd79c5a045cb9eb316249d39f1d82
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-17 16:19:31 -0300

    exercicio 6 em diante, deixando o 5 pra resolver depois e organizar o codigo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-I/exercicio-parte-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-I/exercicio-parte-1.js
@@ -3,6 +3,29 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-console.log('Bem-vinda, ' + info.personagem);
+
+let infoPatinhas = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+}
+
+// console.log('Bem-vinda, ' + info.personagem);
 info['recorrente'] = 'Sim'
-console.log(info);
+infoPatinhas['recorrente'] = 'Sim'
+// console.log(info, infoPatinhas);
+
+for (let key in info){
+    // console.log(key);
+}
+for (let key in info){
+    // console.log(info[key]);
+}
+for (let key in info){
+   if (info.recorrente === infoPatinhas.recorrente){
+       info.recorrente = 'Ambos recorrentes';   
+   }
+    console.log(info[key] + " e " + infoPatinhas[key]);
+}
+console.log(info.recorrente)
+    
