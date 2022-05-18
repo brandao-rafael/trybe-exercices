6b621b99855162ba6a3c6f682fc3ec2c05247484
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-18 11:28:20 -0300

    exercicio 3 e 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-bonus/exercicio-bonus-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-bonus/exercicio-bonus-2.js
@@ -1,38 +1,12 @@
-function fruitsCounter (){
-    const basket = [
-        'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
-        'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
-        'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
-        'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
-        'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
-        'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
-        'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
-        'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
-        'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
-        'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
-        'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
-        'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
-        'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
-        'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
-        'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
-        'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
-        'Banana', 'Pera', 'Abacate', 'Uva',
-    ];
-
-const basket2 = {}
-    for (let i = 0; i < basket.length; i += 1){
-        let fruit = basket[i];
-        if (!basket2[fruit]) {
-            basket2[fruit] = 0;
-            basket2[fruit] += 1;
-        } else {
-            basket2[fruit] += 1;
-        }         
+function arrayOfNumber (){
+    let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+    let pair = [];
+    for (let i = 0; i < vector.length; i += 1){
+        for (let j = 0; j < vector[i].length; j += 1)
+        if (vector[i][j] % 2 === 0){
+            pair.push(vector[i][j])
+        }
     }
-let returnString = ' ';
-for (let key in basket2){
-    returnString += key + ": " + basket2[key] + " \n";
+    return pair;
 }
-    return returnString
-}
-console.log(fruitsCounter());
+console.log(arrayOfNumber())
