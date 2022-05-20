c09e9ea3e70331c7a0cb6622bf16b4e99885c82e
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-20 15:06:22 -0300

    exercicios 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios-conteudo/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios-conteudo/script.js
@@ -1,3 +1,4 @@
+//exercicio 1
 const elementYou = document.getElementById('elementoOndeVoceEsta').parentElement;
 const sonOfSon = document.getElementById('elementoOndeVoceEsta').firstElementChild;
 const sonOfFather = document.getElementById('pai').firstElementChild;
@@ -11,4 +12,20 @@ sonOfFather.innerText = "filho do pai";
 sonOfFatherYou.style.color = "red"; 
 console.log(third);
 thirdFather.innerText = "terceiro filho";
- 
+
+//exercicio 2
+let brotherElement = document.createElement('section');
+brotherElement.innerText = 'elemento irmão'; //Criando elemento irmão
+elementYou.appendChild(brotherElement);
+
+let elementIAm = document.getElementById('elementoOndeVoceEsta');
+let mySon = document.createElement('section');
+mySon.innerText = 'elemento meu filho';
+elementIAm.appendChild(mySon);
+
+let grandSon = document.createElement('section');
+grandSon.innerText = 'elemento meu neto';
+sonOfSon.appendChild(grandSon);
+
+console.log(grandSon.parentElement.lastElementChild.previousElementSibling);
+
