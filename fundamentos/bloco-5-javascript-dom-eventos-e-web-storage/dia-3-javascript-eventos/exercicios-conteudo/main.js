6d493730fd3b66433ef565529f194b7b44bf7325
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-23 19:12:40 -0300

    exercicio 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-conteudo/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-conteudo/main.js
@@ -7,25 +7,39 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre? R: Ocorre pois ela esta com transform no css.
-window.onload = rmClassLi;
-function rmClassLi (){
+window.onload = function () {
     firstLi.classList.remove('tech');
 };
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-// firstLi.addEventListener('click', adcClass(firstLi));
-// secondLi.addEventListener('click', adcClass(secondLi));
-// thirdLi.addEventListener('click', adcClass(thirdLi));
-// function adcClass(param) {
-//     param.className = "tech";
-// }
+function adcClass(param) {
+    param.classList.add('tech');
+}
+firstLi.addEventListener('click', function(){
+    adcClass(firstLi);
+    secondLi.classList.remove('tech');
+    thirdLi.classList.remove('tech');
+});
+secondLi.addEventListener('click',  function(){
+    adcClass(secondLi);
+    firstLi.classList.remove('tech');
+    thirdLi.classList.remove('tech');
+});
+thirdLi.addEventListener('click',  function(){
+    adcClass(thirdLi);
+    firstLi.classList.remove('tech');
+    secondLi.classList.remove('tech');
+});
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-
+function redirect (){
+    myWebpage.addEventListener('dblclick', window.location.href = "https://brandao-rafael.github.io")
+}
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
@@ -33,10 +47,10 @@ function rmClassLi (){
 
 
 function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+    // O Event é passado como um parâmetro para a função.
+    event.target.innerText = 'Opção reiniciada';
+    // O event possui várias propriedades, porém a mais usada é o event.target,
+    // que retorna o objeto que disparou o evento.
 }
 
 firstLi.addEventListener('dblclick', resetText);
