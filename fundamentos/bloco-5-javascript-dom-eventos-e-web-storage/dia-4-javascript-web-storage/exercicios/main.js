a2a788f8c32ef9822e2ba51e904bc7f9f171231b
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-05-25 10:54:54 -0300

    exercicio finalizado

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/main.js
@@ -11,21 +11,32 @@ window.onload = function(){
     const btnFontFamily = document.getElementById('btnFontFamily');
 
     const body = document.querySelector('body');
+    
+    body.style.backgroundColor = localStorage.getItem('background');
+    body.style.color = localStorage.getItem('color');
+    body.style.fontSize = localStorage.getItem('FontSize') + 'px';
+    body.style.lineHeight = localStorage.getItem('lineHeigth') + 'px';
+    body.style.fontFamily = localStorage.getItem('fontFamily');
 
     btnBackground.addEventListener('click', function(){
         body.style.backgroundColor = inputBackground.value;
+        localStorage.setItem("background", inputBackground.value);
     });
     btnColor.addEventListener('click', function (){
         body.style.color = inputColor.value;
+        localStorage.setItem("color", inputColor.value);
     });
     btnFontSize.addEventListener('click', function(){
         body.style.fontSize = inputFontSize.value + 'px';
+        localStorage.setItem("fontSize", inputFontSize.value);
     });
     btnLineHeigth.addEventListener('click', function(){
         body.style.lineHeight = inputLineHeigth.value + 'px';
+        localStorage.setItem("lineHeigth", inputLineHeigth.value);
     });
     btnFontFamily.addEventListener('click', function(){
         body.style.fontFamily = inputFontFamily.value;
+        localStorage.setItem("FontFamily", inputFontFamily.value);
     });
 
 
