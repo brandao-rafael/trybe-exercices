a8aceee1ceff5333b19a62c24d566bb5e48c2d56
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-06-08 18:36:37 -0300

    exercicio 2 da parte 2 com arrow e ternary

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
@@ -1,3 +1,12 @@
+// exercicio 1
 const fatorial = (n) => n === 0 || n === 1 ? 1 : n * fatorial(n-1);
 
 console.log(fatorial(4));
+
+// exercicio 2
+const logestWord = (string) => {
+    let logest = '';
+    string.split(' ').sort(function (a, b) {b.length > a.length ? logest = b : logest = a});
+    return logest;
+}
+console.log(logestWord("A maior palavra"))
