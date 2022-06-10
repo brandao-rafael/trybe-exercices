bbf0b8fbabd4722134d0c352c8f54389f5f9bbbf
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-06-09 23:17:50 -0300

    exercicio 7

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicios-III/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicios-III/script.js
@@ -17,21 +17,33 @@ const lesson3 = {
     professor: 'Maria Clara',
     turno: 'noite',
 };
+
 // Exercicio 1
 const addShift = (lesson, key, value) => lesson[key] = value;
 addShift(lesson2, Object.keys(lesson3)[3], lesson3['turno']);
+
 // Exercicio 2
 const showKeys = (lesson) => Object.keys(lesson);
 // console.log(showKeys(lesson1));
+
 // Exercicio 3
 const showLength = (lesson) => Object.keys(lesson).length;
 // console.log(showLength(lesson3));
+
 // Exercicio 4
 const showValue = (lesson) => Object.values(lesson);
 // console.log(showValue(lesson2));
+
 // Exercicio 5
 const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 // console.log(allLessons);
+
 // Exercicio 6
 const studentNumbers = () => allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes + allLessons.lesson3.numeroEstudantes;
 // console.log(studentNumbers());
+
+// Exercicio 7
+const getValueByNumber = (lesson, n) => {
+    return Object.values(lesson)[n];
+}
+// console.log(getValueByNumber(lesson1, 0));
