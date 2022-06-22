1cd38876b7e340812595d03ce05f2895065a183b
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-06-22 15:32:12 -0300

    Conteúdo do course

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios-conteudo/script.js
@@ -0,0 +1,9 @@
+const wakeUp = () => 'Acordando!!';
+
+const breakFast = () => 'Bora tomar café!!';
+
+const sleep = () => 'Partiu dormir!!';
+
+const doingThings = (func) => func();
+
+console.log(doingThings(wakeUp));
