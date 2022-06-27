10133ff1978588b5871d4c450118e4c76e889ade
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-06-27 15:58:07 -0300

    Exercicio 5 e identação

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios/exercicio1.js
@@ -1,7 +1,7 @@
 const arrays = [
-    ['1', '2', '3'],
-    [true],
-    [4, 5, 6],
+	['1', '2', '3'],
+	[true],
+	[4, 5, 6],
 ];
 
 const flatten = (arr) => arr.reduce((acc, curVal) => acc.concat(curVal), [])
