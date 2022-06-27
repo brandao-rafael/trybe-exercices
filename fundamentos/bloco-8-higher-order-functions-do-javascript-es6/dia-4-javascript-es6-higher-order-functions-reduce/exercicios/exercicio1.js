b0f360a807b30ecf7aa8f4dd98bbad30c125eaff
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-06-27 14:38:07 -0300

    Exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios/exercicio1.js
@@ -0,0 +1,10 @@
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+const flatten = (arr) => arr.reduce((acc, curVal) => acc.concat(curVal), [])
+
+
+console.log(flatten(arrays));
