32cf86f88ddd816068f2a4abb34b3451a09c8ef4
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-07-08 10:40:11 -0300

    Exercicio bonus

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicios/exercicio-2/api.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicios/exercicio-2/api.js
@@ -1,9 +1,16 @@
-const unList = document.getElementById('unList');
-
-const createElement = (coin) => {
+const ul = document.getElementById('unList');
+const modalUl = document.getElementById('modalUl')
+const criptoValue = [];
+const createElement = (coin, currency) => {
     const li = document.createElement('li');
-    li.innerText = `${coin.id} ${coin.symbol}: U$d: ${parseFloat(coin.priceUsd).toFixed(2)}`;
-    unList.appendChild(li);
+    if (currency === 'usd') {
+        li.innerText = `${coin.id} ${coin.symbol}: ${parseFloat(coin.priceUsd).toFixed(2)}`;
+        ul.appendChild(li);
+    } else {
+        li.innerText = `${coin.id} ${coin.symbol}: ${parseFloat(coin.priceUsd * currency).toFixed(2)}`;
+        modalUl.appendChild(li);
+    }
+    
 }
 
 const fetchCripto = () => {
@@ -17,8 +24,37 @@ const fetchCripto = () => {
         .then((response) => response.json())
         .then((data) => {
             data.data.filter((cripto) => cripto.rank <= 10)
-            .forEach((coin) => createElement(coin));
-        })
+            .forEach((coin) => {
+                criptoValue.push(coin);
+                createElement(coin, 'usd')
+            });
+        }).catch((error) => console.log(error));
 }
+
+let modal = document.getElementById('myModal');
+
+const fetchCriptoBrl = () => {
+    const urlBrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
+    const fConfig = {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    };
+
+    fetch(urlBrl, fConfig)
+        .then((response) => response.json())
+        .then((value) => {
+            modal.style.display = "block";
+            const { brl } = value.usd;
+            criptoValue.forEach((coin) => createElement(coin, brl));
+        }).catch((error) => console.log(error));
+}
+const btn = document.getElementById('btn');
+btn.addEventListener('click', fetchCriptoBrl);
+let span = document.getElementsByClassName('close')[0];
+span.onclick = () => {
+    modal.style.display = 'none';
+    modalUl.innerHTML = '';
+}
+
 
 window.onload = fetchCripto;
