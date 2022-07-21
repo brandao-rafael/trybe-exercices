c1722dd6b290c051715638bf252e91fb21aff56b
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-07-21 11:39:41 -0300

    Exercicios do conteúdo

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-conteúdo/introducao-eventos/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-conteúdo/introducao-eventos/src/App.js
@@ -6,20 +6,52 @@ import './App.css';
 class App extends Component{
   constructor() {
     super()
-    this.listenerEvent1 = this.listenerEvent1.bind(this)
-    this.listenerEvent2 = this.listenerEvent2.bind(this)
-    this.listenerEvent3 = this.listenerEvent3.bind(this)
+    this.state = {
+      clicksFirst: 0,
+      clicksSecond: 0,
+      clicksThird: 0, 
+    }
+    this.listenerEvent = this.listenerEvent.bind(this)
     
   }
-  listenerEvent1 = () => console.log('botão 1');
-  listenerEvent2 = () => console.log('botão 2');
-  listenerEvent3 = () => console.log('botão 3');
+  
+  listenerEvent = (btn) => {
+    if (btn === 1 ){ 
+      this.setState((estadoAnterior, _props) => ({
+        clicksFirst: estadoAnterior.clicksFirst + 1
+      }), () => {
+      console.log(`Botão 1: ${this.changeColor(this.state.clicksFirst)}`);
+      });
+    } else if (btn === 2) {
+      this.setState((estadoAnterior, _props) => ({
+        clicksSecond: estadoAnterior.clicksSecond + 1
+      }), () => {
+        console.log(`Botão 2: ${this.changeColor(this.state.clicksSecond)}`);
+      });
+    } else {
+      this.setState((estadoAnterior, _props) => ({
+        clicksThird: estadoAnterior.clicksThird + 1
+      }), () => {
+        console.log(`Botão 3: ${this.changeColor(this.state.clicksThird)}`);
+      })
+    }
+  }
+
+  changeColor = (value) => {
+    if (value % 2 === 0) {
+      return 'green'
+    } else {
+      return 'white'
+    }
+  }
+
   render (){
+    const { clicksFirst, clicksSecond, clicksThird } = this.state;
     return (
       <div className="App">
-        <button onClick={this.listenerEvent1}>first button</button>
-        <button onClick={this.listenerEvent2}>second button</button>
-        <button onClick={this.listenerEvent3}>third button</button>
+        <button onClick={() => this.listenerEvent(1)} style={{backgroundColor: this.changeColor(clicksFirst)}}>{`cliques: ${this.state.clicksFirst}`}</button>
+        <button onClick={() => this.listenerEvent(2)} style={{backgroundColor: this.changeColor(clicksSecond)}}>{`cliques: ${this.state.clicksSecond}`}</button>
+        <button onClick={() => this.listenerEvent(3)} style={{backgroundColor: this.changeColor(clicksThird)}}>{`cliques: ${this.state.clicksThird}`}</button>
       </div>
     );
 
