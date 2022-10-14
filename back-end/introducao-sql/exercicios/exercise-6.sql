d8efb6593dc61ff4fe48b5f10d49f00d828a3e8a
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-14 16:22:32 -0300

    Exercicios do dia

---
+++ back-end/introducao-sql/exercicios/exercise-6.sql
@@ -0,0 +1,3 @@
+-- Escreva uma query para exibir o nome como “Nome do Projeto” e as horas como “Tempo de Trabalho” de cada projeto.
+USE Scientists;
+SELECT Name AS 'Nome do projeto', Hours AS 'Tempo de trabalho' FROM Projects;
