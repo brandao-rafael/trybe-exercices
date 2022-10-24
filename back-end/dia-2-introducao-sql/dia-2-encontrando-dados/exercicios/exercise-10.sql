e0541322122c5f1599070da2d6f07525a641e7b2
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-24 19:42:42 -0300

    Exercicios do dia

--- back-end/dia-2-introducao-sql/dia-2-encontrando-dados/exercicios/exercise-10.sql
+++
@@ -1,4 +0,0 @@
--- Escreva uma query para exibir o nome e as horas dos três projetos com a maior quantidade de horas.
-USE Scientists;
-SELECT * FROM Projects
-ORDER BY Hours DESC LIMIT 3;
--- back-end/dia-2-introducao-sql/dia-2-encontrando-dados/exercicios/exercise-15.sql
+++
@@ -1,3 +0,0 @@
--- Escreva uma query que exiba a string “Existem Number cientistas na tabela Scientists.”, em que Number se refira a quantidade de cientistas.
-USE Scientists;
-SELECT CONCAT("Existem ", COUNT(Name), " cientistas na tabela Scientists") FROM Scientists;
