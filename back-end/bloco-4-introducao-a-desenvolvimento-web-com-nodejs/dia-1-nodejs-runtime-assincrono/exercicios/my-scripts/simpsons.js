b71906b70c3738d480ec90867705e3ad3ace2b0f
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-28 11:50:17 -0300

    exercicios 6e e 6f

--- back-end/bloco-4-introducao-a-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/my-scripts/simpsons.js
+++ back-end/bloco-4-introducao-a-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/my-scripts/simpsons.js
@@ -33,11 +33,38 @@ const createSimpsonFamily = async () => {
   await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily));
 }
 
+const fs = require('fs').promises;
+
+async function addNelsonToFamily() {
+  const fileContent = await fs
+    .readFile('./simpsonsFamily.json', 'utf-8');
+  
+  const simpsonsFamily = JSON.parse(fileContent);
+  simpsonsFamily.push({ id: '8', name: 'Nelson Muntz' });
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
+}
+
+async function replaceNelson() {
+  const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+
+  const simpsonsWithoutNelson = simpsons.filter((simpson) => simpson.id !== '8');
+
+  const simpsonsWithMaggie = simpsonsWithoutNelson
+    .concat([{ id: '15', name: 'Maggie Simpson' }]);
+  // com spread seria assim: 
+  // const simpsonsWithMaggie = [ ...simpsonsWithoutNelson, simpsonsWithMaggie ]
+
+  return fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie));
+}
+
 const main = async () => {
   const simpson = await getSimpsonsById(1);
   const filteredSimpson = await filterSimpson();
-  readAll();
-  createSimpsonFamily(); // 6D
+  await readAll();
+  await createSimpsonFamily();
+  await addNelsonToFamily();
+  await replaceNelson();
   console.log(`by id`, simpson);
   console.log(`filtered`, filteredSimpson);
 }
