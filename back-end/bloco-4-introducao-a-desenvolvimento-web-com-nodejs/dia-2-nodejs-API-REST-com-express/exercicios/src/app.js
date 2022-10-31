5c2fb688125944d2d52a1ff5a84eef07ff248f7a
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-31 12:12:27 -0300

    adicionando arquivos

--- back-end/bloco-4-introducao-a-desenvolvimento-web-com-nodejs/dia-2-nodejs-API-REST-com-express/exercicios/src/app.js
+++ back-end/bloco-4-introducao-a-desenvolvimento-web-com-nodejs/dia-2-nodejs-API-REST-com-express/exercicios/src/app.js
@@ -45,6 +45,36 @@ app.post('/movies', async (req, res) => {
       price,
     };
     const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((element) => element.id === Number(id));
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await readFile();
+    const filteredMovies = movies.filter((movie) => movie.id !== Number(id));
+    const updatedMovies = JSON.stringify(filteredMovies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(204).end();
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
