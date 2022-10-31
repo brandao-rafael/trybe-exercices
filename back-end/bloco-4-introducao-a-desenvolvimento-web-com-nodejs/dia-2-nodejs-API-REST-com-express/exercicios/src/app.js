fa1b65d66e449425f78b31e6a2a75254f72d9fbb
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-31 12:11:21 -0300

    Exercicios do dia

---
+++ back-end/bloco-4-introducao-a-desenvolvimento-web-com-nodejs/dia-2-nodejs-API-REST-com-express/exercicios/src/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pode ser lido: ${error}`);
+  }
+};
+
+app.get('movies/:id', async (req, res) => {
+  try {
+    const movies = await readFile();
+    const movie = movies.find(({ id }) => id === Number(req.params.id));
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get('/movies', async (_req, res) => {
+  try {
+    const movies = await readFile();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = app;
