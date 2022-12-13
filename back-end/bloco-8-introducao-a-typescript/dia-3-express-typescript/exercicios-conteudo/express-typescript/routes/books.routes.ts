a1063195d9771ffcf52313f020a602d2ae351f83
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-12-13 15:56:13 -0300

    Exercicios conteudo e para fixar

---
+++ back-end/bloco-8-introducao-a-typescript/dia-3-express-typescript/exercicios-conteudo/express-typescript/routes/books.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import BooksController from '../controllers/book.controller';
+import BookService from '../services/books.service';
+
+const router = Router();
+
+const booksController = new BooksController();
+
+const booksSlashId = '/books/:id';
+
+router.get('/books', booksController.getAll);
+
+router.get(booksSlashId, booksController.getById);
+
+router.post('/books', booksController.create);
+
+router.put(booksSlashId, booksController.update);
+
+router.delete(booksSlashId, booksController.remove);
+
+router.patch(booksSlashId, booksController.partialUpdate);
+
+export default router;
