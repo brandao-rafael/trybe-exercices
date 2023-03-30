7096f2f4c084367a934aeadc119989ded7c7e7f8
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-03-30 18:11:38 -0300

    feat: exercicios conteudo

---
+++ ciencia-da-computacao/padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicios-conteudo/src/strategy/BradescoStrategy.py
@@ -0,0 +1,8 @@
+from BancoStrategy import BancoStrategy
+
+
+class BradescoStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Bradesco (exemplo)
+        print("Sucesso!")
---
+++ ciencia-da-computacao/padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicios-conteudo/src/strategy/ItauStrategy.py
@@ -0,0 +1,8 @@
+from BancoStrategy import BancoStrategy
+
+
+class ItauStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Itau (exemplo)
+        print("Débito realizado pelo Itau")
